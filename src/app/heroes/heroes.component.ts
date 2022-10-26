f6b192c9ed870d808b39de623bc30fccbd0d223a
Arturo <arturocecena@hotmail.com>
2022-10-25 22:20:49 -0600

    Integrando correctamente la lista desplegable (punto 2 TOH)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes';
 
 
 @Component({
@@ -14,6 +15,13 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
 
   };
+
+  heroes = HEROES;
+  
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 
   constructor() {
     
